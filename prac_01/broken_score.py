acca92007bb7d5f89ca30206315ec2394118ed67
JiangJinsong <jinsong.jiang@my.jcu.edu.au>
2025-02-07 14:29:05 +0800

    print broken score

---
+++ prac_01/broken_score.py
@@ -0,0 +1,22 @@
+"""
+Start
+get score
+if score < 0 or score > 100
+    print "Invalid score"
+else if score >= 90
+    print "Excellent"
+else if score >= 50
+    print "Passable"
+else
+    print "Bad"
+End
+"""
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
